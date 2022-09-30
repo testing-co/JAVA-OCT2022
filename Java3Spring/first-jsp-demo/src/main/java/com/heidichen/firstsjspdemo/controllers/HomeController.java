8f4c65a6f7bc368b6fd4ef500549a4046a35b13f
heidi1105 <heidi1105@gmail.com>
2022-09-30 15:18:38 -0700

    jstl

--- Java3Spring/first-jsp-demo/src/main/java/com/heidichen/firstsjspdemo/controllers/HomeController.java
+++ Java3Spring/first-jsp-demo/src/main/java/com/heidichen/firstsjspdemo/controllers/HomeController.java
@@ -1,13 +1,43 @@
 package com.heidichen.firstsjspdemo.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // allows you to display jsp file as the view
 public class HomeController {
 
 	@RequestMapping("/home")
 	public String home() {
 		return "home.jsp";
+	}
+	
+	@RequestMapping("/jstl")
+	public String renderingDataDemo(Model model) {
+		String name = "Alex Miller";
+		Integer age = 14;
+		boolean isHungry = true;;
+		String hackerScript = "<script>alert('hi')</script>";
+		model.addAttribute("jspName", name);
+		model.addAttribute("jspAge", age);
+		model.addAttribute("jspHungry", isHungry);
+		model.addAttribute("hackerScript", hackerScript);
+		
+		ArrayList<String> skills = new ArrayList<>();
+		skills.add("debugging skills");
+		skills.add("barista skills");
+		skills.add("breaking the code");
+		skills.add("losing braincell");
+		model.addAttribute("jspSkills", skills);
+		
+		for(String eachSkill : skills) {
+			System.out.println(eachSkill);
+		}
+		
+		
+		
+ 		return "jstlDemo.jsp";
 	}
 }
---
+++ Java3Spring/first-jsp-demo/src/main/webapp/WEB-INF/jstlDemo.jsp
@@ -0,0 +1,63 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Demo for rendering data</h1>
+	<h3>
+		Name: ${jspName }
+		<c:out value="${jspName }" />
+	</h3>
+	<h3>Age: ${jspAge }</h3>
+	<h3>Is Hungry?: ${jspHungry }</h3>
+
+	<p>
+		<c:out value="${hackerScript }" />
+	</p>
+	<h1>If-statement</h1>
+	<c:if test="${jspHungry == true }">
+		<p>I am hungry</p>
+	</c:if>
+	
+	<h1>If-else statement</h1>
+	<c:choose>
+		<c:when test="${jspAge >50 }">
+			<p> You are older than 50 years old </p>
+		</c:when>
+		<c:otherwise>
+			<p> You are younger than 50 years old </p>
+		</c:otherwise>
+	</c:choose>
+	
+	<h1>For-loop</h1>
+
+	<ul>
+		<c:forEach var="eachSkill" items="${ jspSkills}">
+			<li> ${eachSkill }</li>
+		</c:forEach>
+	</ul>
+	
+	
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
